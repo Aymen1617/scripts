c05967c25f7543be313fd4c5b635026c24ea0730
Aymen1617 <aymenbaig1617@gmail.com>
2026-01-05 15:51:58 +0500

    Update merge.py

--- merge.py
+++ merge.py
@@ -263,7 +263,7 @@ with DAG(
     dag_id="snapshot_retry_dag",
     default_args=DEFAULT_ARGS,
     description="Retry snapshots from Strapi",
-    schedule="49 15 * * *",  # adjust as needed
+    schedule="* * * * *",  # adjust as needed
     start_date=datetime(2026, 1, 5),
     catchup=False,
     max_active_runs=1,
@@ -276,4 +276,5 @@ with DAG(
     )
 
     run_snapshots_task
+
 
